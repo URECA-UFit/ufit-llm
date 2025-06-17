1318ab303f1fe9f6c6fa6c6c96f0ce08267b7362
황지연 <ghkdwldus0807@gmail.com>
2025-06-17 09:34:03 +0900

    fix: recommend response dto 수정 (#32)
    * fix: AnswerType enum 추가 및 RecommendResponse 변수명 수정
    
    * fix: RecommendResponse 구조 수정 및 code formatting

--- ufit/dto/recommend.py
+++ ufit/dto/recommend.py
@@ -1,16 +1,26 @@
 from pydantic import BaseModel
 from typing import List
+from enum import Enum
+
+
+class AnswerType(str, Enum):
+    RECOMMEND = "RECOMMEND"
+    GENERAL = "GENERAL"
+
 
 class RecommendRequest(BaseModel):
     userId: int
     content: str
     chatRoomId: int
 
+
 class PlanDTO(BaseModel):
     planId: str
     name: str
 
+
 class RecommendResponse(BaseModel):
-    messagesId: str
+    messageId: str
     answer: str
-    recommendPlan: List[PlanDTO]
+    recommendPlans: List[PlanDTO]
+    answerType: AnswerType
