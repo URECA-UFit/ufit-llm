a4ebfb73decd1a02b81f6f4a3abd7d5d78b024f2
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-06-17 13:30:25 +0900

    refector: test 코드 주석 변경

--- ufit/tests/test_rateplan_routes.py
+++ ufit/tests/test_rateplan_routes.py
@@ -38,14 +38,14 @@ def test_embedding_create_and_delete_with_mock():
     delete_response = client.delete(f"/api/admin/rateplans/{dummy_id}")
     assert delete_response.status_code == 200
 
-    # ❌ [실패] 존재하지 않는 ID로 임베딩 생성
+    # 벡터 임베딩 생성 실패 테스트 : 존재하지 않는 ID로 임베딩 생성
     fake_id = str(ObjectId())  # 유효한 ObjectId지만 MongoDB에는 없음
     fail_response = client.post(f"/api/admin/rateplans/{fake_id}")
     assert fail_response.status_code == 404
     assert fail_response.json()["errorCode"] == "RATE_PLAN_NOT_FOUND"
     assert fail_response.json()["message"] == "요금제를 찾을 수 없습니다."
 
-    # ❌ [실패] 존재하지 않는 ID로 임베딩 삭제 (삭제는 내부적으로 실패하지 않으면 성공 처리)
+    # 벡터 임베딩 삭제 실패 테스트 :  존재하지 않는 ID로 임베딩 삭제 (삭제는 내부적으로 실패하지 않으면 성공 처리)
     with patch("ufit.services.embedding_service.vectorstore.delete") as mock_delete:
         mock_delete.side_effect = Exception("강제 삭제 실패")
         response = client.delete(f"/api/admin/rateplans/{dummy_id}")
