798ded6e4c977d60f8f7a716ffc6e93f1fcef4d5
dudxo <father8475@naver.com>
2025-06-23 17:45:42 +0900

    feat: 챗봇 리뷰 사용자 질의 프롬프트 엔지니어링 고도화

--- ufit/services/chat_bot_review_user_query.py
+++ ufit/services/chat_bot_review_user_query.py
@@ -35,11 +35,10 @@ def get_user_queries_after_recommendation(mongo_db: Database, chat_room_id: int,
     user_msgs_to_summarize = []
     found_start_recommendation = False
 
-    for msg in messages:
+    for idx, msg in enumerate(messages):
         msg_content = msg.get("content", "")
         msg_owner = msg.get("owner", False)
-        is_recommendation_msg = (not msg_owner and "추천" in msg_content)
-        
+
         if msg["_id"] == recommendation_obj_id:
             found_start_recommendation = True
             continue
@@ -47,7 +46,9 @@ def get_user_queries_after_recommendation(mongo_db: Database, chat_room_id: int,
         if not found_start_recommendation:
             continue
 
-        if is_recommendation_msg:
+        # break 조건: 챗봇의 RECOMMENDATION_LIST만
+        is_previous_bot_recommendation = not msg_owner and "RECOMMENDATION_LIST" in msg_content
+        if is_previous_bot_recommendation:
             break
         
         if msg_owner:
@@ -56,6 +57,7 @@ def get_user_queries_after_recommendation(mongo_db: Database, chat_room_id: int,
         if len(user_msgs_to_summarize) == limit:
             break
             
+    print(f"[요약 전] LLM에 전달될 메시지 리스트: {user_msgs_to_summarize}", flush=True)
     return user_msgs_to_summarize
 
 
@@ -78,7 +80,21 @@ def summarize_user_queries_with_llm(user_msgs: List[str]) -> str:
         ),
         HumanMessagePromptTemplate.from_template(
             "아래는 사용자의 최근 대화 기록입니다:\n\n{user_queries}\n\n이 대화를 바탕으로 사용자가 어떤 통신 성향을 가지고 있는지, 마치 사용자가 자신의 성향을 한 번에 설명하는 것처럼 자연스러운 하나의 문단으로 요약해주세요. **입력 데이터에 있는 내용만을 요약하고, 추측하지 마세요. '사용자는~', '따라서~'와 같은 설명조 표현을 사용하지 마세요.**"
+            "반드시 요금제 또는 통신 성향 관련 질문만 요약하세요. 그렇지 않은 질문은 요약하지마세요."
+            "[예시1]"
+            "[질문1] 나는 멍청해"
+            "[질문2] 나는 데이터를 많이 써"
+            "[질문3] 오늘 날씨가 좋다"
+            "[질문4] 5만원 이하 요금제 추천해줘"
+            "[답변] 데이터를 많이 쓰는 편이야. 5만원 이하로 알맞은 요금제를 추천해줘\n"
             "반드시 예시와 비슷하게 답변하세요"
+            "[예시]"
+            "[질문1] 나는 유튜브를 많이 봐"
+            "[질문2] 나는 데이터가 좀 부족해"
+            "[질문3] 현재 데이터가 30GB인 요금제를 쓰고 있어"
+            "[질문4] 나에게 맞는 요금제 추천해줘"
+            "[답변] 유튜브를 많아봐서 데이터가 부족해. 현재 요금제는 30GB 데이터를 쓸 수 있는데 이 요금제 말고 알맞는 요금제를 추천해줘\n"
+            "예시처럼 답변하세요"
         ),
     ])
     rendered_msgs = prompt.format_prompt(user_queries=joined).to_messages()
